bb291dfabb41b0395fc69389ec1ca5c6c0382559
kodlee yin <kodlee@fru1t.me>
2018-05-09 00:14:43 -0700

    Add graceful shutdown on sigkill

--- src/main/kotlin/me/fru1t/stak/server/StakServer.kt
+++ src/main/kotlin/me/fru1t/stak/server/StakServer.kt
@@ -7,13 +7,29 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
-  embeddedServer(Netty, 8080) {
+  val engine = embeddedServer(Netty, 8080) {
     routing {
       get("/") {
         call.respondText("Hello world!", ContentType.Text.Html)
       }
     }
-  }.start(wait = true)
+  }
+  Runtime.getRuntime().addShutdownHook(object : Thread() {
+    override fun run() {
+      println("Stopping server...")
+      engine.stop(5, 10, TimeUnit.SECONDS)
+      println("Server was stopped gracefully! Goodnight.")
+    }
+  })
+  println("Starting Stak server...")
+  engine.start(wait = true)
+}
+
+object StakServer {
+  fun start() {
+    main(emptyArray())
+  }
 }
