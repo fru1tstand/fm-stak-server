f08dcfe54cd776684df3d484d974a6dcf431e28c
kodlee yin <kodlee@fru1t.me>
2018-06-09 12:15:54 -0700

    Create ApplicationCall response helper methods
    Added ApplicationCall#respondResult which handles Result-based responses
    (ie. an internal controller result) and transforms it into a request
    response.
    Added ApplicationCall#respondEmpty which responds to a request with just
    an HttpStatusCode. An empty body and Plain content type are appended.
    
    Test: ./gradlew clean test

---
+++ src/main/kotlin/me/fru1t/stak/server/ktor/ApplicationCallUtils.kt
@@ -0,0 +1,34 @@
+package me.fru1t.stak.server.ktor
+
+import io.ktor.application.ApplicationCall
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.isSuccess
+import io.ktor.response.respondText
+import me.fru1t.stak.server.models.Result
+
+/**
+ * Responds to a request using a [Result]. Http status will always be set to the
+ * [Result.httpStatusCode]. If the [result] was successful, the response body will be set to the
+ * output of [successText] with content type [successContentType]. If the [result] was unsuccessful,
+ * the response body will be an empty string with the content type [ContentType.Text.Plain].
+ */
+suspend fun ApplicationCall.respondResult(
+    result: Result<*>,
+    successText: () -> String = { result.value?.toString() ?: "" },
+    successContentType: ContentType = ContentType.Text.Plain) {
+  if (result.httpStatusCode.isSuccess()) {
+    respondText(
+        text = successText(), contentType = successContentType, status = result.httpStatusCode)
+  } else {
+    respondText(text = "", contentType = ContentType.Text.Plain, status = result.httpStatusCode)
+  }
+}
+
+/**
+ * Responds to a request with [httpStatusCode] passing an empty body and a
+ * [ContentType.Text.Plain] content-type.
+ */
+suspend fun ApplicationCall.respondEmpty(httpStatusCode: HttpStatusCode = HttpStatusCode.OK) {
+  respondText(text = "", contentType = ContentType.Text.Plain, status = httpStatusCode)
+}
---
+++ src/test/kotlin/me/fru1t/stak/server/ktor/ApplicationCallUtilsTest.kt
@@ -0,0 +1,87 @@
+package me.fru1t.stak.server.ktor
+
+import com.google.common.truth.Truth.assertThat
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.testing.TestApplicationCall
+import io.ktor.server.testing.contentType
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import me.fru1t.stak.server.models.Result
+import org.junit.jupiter.api.Test
+
+class ApplicationCallUtilsTest {
+  companion object {
+    fun TestApplicationCall.assert(
+        requestHandled: Boolean,
+        contentType: ContentType,
+        content: String?,
+        status: HttpStatusCode) {
+      assertThat(this.requestHandled).isEqualTo(requestHandled)
+      assertThat(this.response.contentType().contentType).contains(contentType.contentType)
+      assertThat(this.response.content).isEqualTo(content)
+      assertThat(this.response.status()).isEqualTo(status)
+    }
+  }
+
+  @Test fun respondResult_default() = withTestApplication {
+    val responseResult = Result("This is a successful result")
+    application.routing {
+      get("/") { call.respondResult(responseResult) }
+    }
+
+    val result = handleRequest(HttpMethod.Get, "/")
+
+    result.assert(true, ContentType.Text.Plain, responseResult.value, HttpStatusCode.OK)
+  }
+
+  @Test fun respondResult_success() = withTestApplication {
+    val responseText = "[ \"test response\" ]"
+    val responseType = ContentType.Application.Json
+    val responseResult =
+      Result(value = "This is a successful result", httpStatusCode = HttpStatusCode.Accepted)
+    application.routing {
+      get("/") { call.respondResult(responseResult, { responseText }, responseType) }
+    }
+
+    val result = handleRequest(HttpMethod.Get, "/")
+
+    result.assert(true, responseType, responseText, responseResult.httpStatusCode)
+  }
+
+  @Test fun respondResult_notSuccess() = withTestApplication {
+    val responseResult = Result<Any>(httpStatusCode = HttpStatusCode.Unauthorized)
+    application.routing {
+      get("/") { call.respondResult(responseResult) }
+    }
+
+    val result = handleRequest(HttpMethod.Get, "/")
+
+    result.assert(true, ContentType.Text.Plain, "", HttpStatusCode.Unauthorized)
+  }
+
+  @Test fun respondEmpty_default() = withTestApplication {
+    application.routing {
+      get("/") { call.respondEmpty() }
+    }
+
+    val result = handleRequest(HttpMethod.Get, "/")
+
+    result.assert(true, ContentType.Text.Plain, "", HttpStatusCode.OK)
+  }
+
+  @Test fun respondEmpty() = withTestApplication {
+    val responseStatus = HttpStatusCode.Continue
+    application.routing {
+      get("/") { call.respondEmpty(responseStatus) }
+    }
+
+    val result = handleRequest(HttpMethod.Get, "/")
+
+    result.assert(true, ContentType.Text.Plain, "", responseStatus)
+  }
+}
