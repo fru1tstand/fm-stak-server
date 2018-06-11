8a792356fc06187c43642d017f903c72817324e3
kodlee yin <kodlee@fru1t.me>
2018-06-11 00:20:29 -0700

    Add test coverage for Result
    Test: ./gradlew clean test

--- src/test/kotlin/me/fru1t/stak/server/models/ResultTest.kt
+++ src/test/kotlin/me/fru1t/stak/server/models/ResultTest.kt
@@ -4,11 +4,26 @@ import com.google.common.truth.Truth.assertThat
 import io.ktor.http.HttpStatusCode
 import org.junit.jupiter.api.Test
 
-class ResultTest {
+class LegacyResultTest {
   @Test fun default() {
     val result = LegacyResult<Any>()
 
     assertThat(result.value).isNull()
     assertThat(result.httpStatusCode).isEqualTo(HttpStatusCode.OK)
+  }
+}
+
+class ResultTest {
+  /** Test enum that implements [StatusEnum]. */
+  enum class TestStatusEnum(val httpStatusCode: HttpStatusCode) : StatusEnum {
+    TEST_VALUE(HttpStatusCode.OK);
+    override fun getStatus(): HttpStatusCode = httpStatusCode
+  }
+
+  @Test fun default() {
+    val result = Result("Test Value", TestStatusEnum.TEST_VALUE)
+
+    assertThat(result.value).isEqualTo("Test Value")
+    assertThat(result.status).isEqualTo(TestStatusEnum.TEST_VALUE)
   }
 }
